a0e29273b2f0fd934797c9ce1e14d38e70aa2a14
Bricio444 <fabriciiferreiracezario@gmail.com>
2021-10-26 19:06:26 +0000

    atualização

--- src/main/java/application/controllers/LivroController.java
+++ src/main/java/application/controllers/LivroController.java
@@ -2,6 +2,8 @@ package application.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequesteMethod;
+import org.springframework.web.bind.annotation.requesteParam;
 
 import application.repositories.LivroRepository;
 
@@ -25,4 +27,15 @@ public class LivroController {
     return "insert.jsp";
   }
 
+
+  @RequestMapping("method"=RequesteMethod.POST)
+  public String  saveInsert(@requesteParam("titulo")String titulo){
+   livro livro= new livro();
+   livro.strtitulo(livro);
+   livro setTitulo(titulo);
+   livrosRepo.save(livro);
+   return "redirect:/livro/list";
+
+  
+  }
 }
